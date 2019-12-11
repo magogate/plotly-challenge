e57f33d3e5766f09646b395c45a722ada168f91a
mandargogate <mandargogate@outlook.com>
2019-12-11 00:23:18 -0500

    Added bubble chart

--- Belly_Button_Diversity/static/js/main.js
+++ Belly_Button_Diversity/static/js/main.js
@@ -87,12 +87,41 @@ function populatePieChart(name){
     Plotly.newPlot('bar', data, layout);
 }//populatePieChart
 
+function populateBubbleChart(name){
+    // https://plot.ly/javascript/bubble-charts/
+    let samples = myJsonObj.samples.filter(function(d){
+        return d.id == name;
+    })
+
+    let trace1 = {
+        x: samples[0].otu_ids,
+        y: samples[0].sample_values,
+        text: samples[0].otu_labels,
+        mode: 'markers',
+        marker: {
+          color: samples[0].otu_ids,
+          size: samples[0].sample_values
+        }
+      };
+    
+    var data = [trace1];
+
+    var layout = {
+        title: 'Bubble Chart Hover Text',
+        showlegend: false,
+        height: 600,
+        width: 1200
+    };
+
+    Plotly.newPlot('bubble', data, layout);
+}
 
 let cmbSubNo = d3.select("#selDataset");
 cmbSubNo.on("change", function(d){
     let selectedName = d3.select("#selDataset").property("value")
     populateDemographicInfo(selectedName);
     populatePieChart(selectedName);
+    populateBubbleChart(selectedName)
 });
 
 loadJson()
